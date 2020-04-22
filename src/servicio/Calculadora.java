3984f8e7c87b82092d579a1a17d1b93f1fc8799a
sponce <poncesaul014@outlook.com>
2020-04-21 22:22:29 -0600

    First Commit

---
+++ src/servicio/Calculadora.java
@@ -0,0 +1,20 @@
+package servicio;
+
+public class Calculadora {
+
+	public int suma(int x, int y){
+		return x+y;
+	}
+	
+	public int resta (int x, int y){
+		return x-y;
+	}
+	
+	public int multiplicar(int x, int y){
+		return x*y;
+	}
+	
+	public int divide(int x, int y){
+		return x/y;
+	}
+}
